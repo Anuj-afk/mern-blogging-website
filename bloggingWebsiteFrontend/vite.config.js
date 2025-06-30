3d626f97f1624f7646d79a9b9b570009d21bfe19
Anuj-afk <anujg6263@gmail.com>
2025-06-30 11:31:31 +0530

    changed vite config

--- bloggingWebsiteFrontend/vite.config.js
+++ bloggingWebsiteFrontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"/mern-blogging-website/",
+  server: {
+        historyApiFallback: true, // Handles routing for client-side applications
+  },
 })
